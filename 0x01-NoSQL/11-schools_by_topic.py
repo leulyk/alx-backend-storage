d23d9657e5b3088b6708f8a3679eadfa845a9e21
Leul Yiheyis <leulykpro@gmail.com>
2022-04-26 21:55:29 +0300

    check if key exists before testing against topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -9,6 +9,7 @@ def schools_by_topic(mongo_collection, topic):
     """ returns a list of schools teaching a specified topic """
     matches = []
     for school in mongo_collection.find():
-        if "Python" in school["topics"]:
-            matches.append(school)
+        if "topics" in school.keys():
+            if "Python" in school["topics"]:
+                matches.append(school)
     return matches
