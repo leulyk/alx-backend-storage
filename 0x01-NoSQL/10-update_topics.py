1ebcc279d418b2b856b93db59525039fdf1b83f9
Leul Yiheyis <leulykpro@gmail.com>
2022-04-26 21:37:56 +0300

    update school topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+    PyMongo: Exercise 3
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """ changes all topics of a school document """
+    mongo_collection.update_many(
+        {"name": name},
+        {
+            "$set": {"topics": topics}
+        },
+        True
+    )
