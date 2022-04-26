63d5abce7df7be19a212907297eb45d822baaa78
Leul Yiheyis <leulykpro@gmail.com>
2022-04-26 21:52:10 +0300

    remove unnecessary line

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -9,7 +9,6 @@ def schools_by_topic(mongo_collection, topic):
     """ returns a list of schools teaching a specified topic """
     matches = []
     for school in mongo_collection.find():
-        print(school)
         if "Python" in school["topics"]:
             matches.append(school)
     return matches
