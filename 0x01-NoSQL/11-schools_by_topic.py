648fcd52f783664621de9b07f1964b68c0dd9526
Leul Yiheyis <leulykpro@gmail.com>
2022-04-27 04:38:46 +0300

    use the find method to filter and an alteernate implementation

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -7,9 +7,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     """ returns a list of schools teaching a specified topic """
-    matches = []
-    for school in mongo_collection.find():
-        if "topics" in school.keys():
-            if "Python" in school["topics"]:
-                matches.append(school)
-    return matches
+    return [school for school in mongo_collection.find({"topics": topic})]
