3e38c2d043541c666151c61393753e73804503aa
Leul Yiheyis <leulykpro@gmail.com>
2022-04-27 20:41:47 +0300

    define the Cache class and the store method

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+    Module to learn Redis
+"""
+
+import redis
+from typing import List, Union
+from uuid import uuid4
+
+
+class Cache:
+    """ blueprint for a redis data storage """
+    def __init__(self) -> None:
+        """ initializes a Cache instance """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[int, float, str, bytes, List]) -> str:
+        """ stores a key-value pair and returns the key """
+        key = str(uuid4())
+        self._redis.set({key: data})
+        return key
